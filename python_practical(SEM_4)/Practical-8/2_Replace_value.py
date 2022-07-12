23f7a953026f448e098bb0401b627b649076f348
aashaykolhe <92323381+aashaykolhe@users.noreply.github.com>
2022-07-13 03:08:35 +0530

    Add files via upload

---
+++ python_practical(SEM_4)/Practical-8/2_Replace_value.py
@@ -0,0 +1,9 @@
+# Program  to replace values from the dictionary
+
+D = {"book":20,"pen":15,"ink":18,"pencil":22}
+
+print(D)
+
+D["pen"]=5
+print("After replacing value")
+print(D)
---
+++ python_practical(SEM_4)/Practical_1/8_c_to_f.py
@@ -0,0 +1,6 @@
+# Program to convert temperatures from Celcius to Farenheit
+
+c=int(input("Enter the temperature in Celcius: "))
+#formula
+f=c*9/5+32
+print(c,"Celcius is equal to ",f,"Fahrenheit")
